7b8955ef3ccfff7659b8069f9ab58d90dce699ec
144134 <144134@3VJ3S93>
2022-02-25 20:17:32 +0530

    Added Leave type repository, end of repository pattern module

---
+++ LeaveManagement.Web/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Web.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
+ 
